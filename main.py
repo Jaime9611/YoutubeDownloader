2e51907857e953441d1c987a09dae1633f628afd
Jaime9611 <jaimeescobar9611@gmail.com>
2020-03-09 18:06:28 -0500

    Basic UI Finish

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from tkinter import *
 from tkinter import ttk
 
+from PIL import ImageTk, Image
+
 
 # tkinter object
 root = Tk()
 
 # root configurations
-root.title("Feet to Meters")
+root.title("YouTube Downloader")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-# main content
+# # main content
 content = ttk.Frame(root, padding=(3,3,12,12))
 content.grid(column=0, row=0, sticky=(N, E, S, W))
-content['height'] = 400
-content['width'] = 700
+content.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+
+# add logo
+img = Image.open('./images/ytlogo.png')
+image = img.resize((300, 300), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+imgobj = ImageTk.PhotoImage(image)
+img_label = ttk.Label(content, image=imgobj)
+img_label.grid(column=0, row=0, sticky=(W, E), padx=100)
+
+
+# entry
+entry = ttk.Entry(content, textvariable='')
+entry.grid(column=0, row=1, sticky=(W, E), columnspan=2, padx=10)
+
+
+
 
 root.mainloop() # run the GUI
 
