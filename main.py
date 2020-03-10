7dc7840ddbe5395053e58c203c43340c5a91625a
Jaime9611 <jaimeescobar9611@gmail.com>
2020-03-09 19:02:25 -0500

    Can download videos

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 
 from PIL import ImageTk, Image
 
+from ydownloader import download_video
+
 
 # tkinter object
 root = Tk()
@@ -28,9 +30,10 @@ img_label.grid(column=0, row=0, sticky=(W, E), padx=100)
 
 
 # entry
-entry = ttk.Entry(content, textvariable='')
+link = StringVar()
+entry = ttk.Entry(content, textvariable=link)
 entry.grid(column=0, row=1, sticky=(W, E), columnspan=2, padx=10)
-
+root.bind('<Return>', lambda i: download_video(entry.get()))
 
 
 
--- ydownloader.py
+++ ydownloader.py
@@ -2,19 +2,20 @@ from pytube import YouTube
 
 
 # Give the link:
-link = 'https://www.youtube.com/watch?v=2Vv-BfVoq4g'
+# link = 'https://www.youtube.com/watch?v=2Vv-BfVoq4g'
 
-try:
-    yt = YouTube(link) # YouTube object
-except:
-    print('Connection Error.')
+def download_video(link):
+    try:
+        yt = YouTube(link) # YouTube object
+    except:
+        print('Connection Error.')
 
-stream = yt.streams.filter(progressive=True).first()
+    stream = yt.streams.filter(progressive=True).first()
 
 
-try:
-    stream.download()
-except:
-    print('Some Error.')
+    try:
+        stream.download()
+    except:
+        print('Some Error.')
 
-print('Download Completed...')
+    print('Download Completed...')
